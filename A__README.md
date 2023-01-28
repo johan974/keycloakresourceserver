d829b290db92ccae62138a2d96c258056b9bd69f
johan974 <jj974@upcmail.nl>
2023-01-28 20:17:04 +0100

    Tweede settings voor tweede server

---
+++ A__README.md
@@ -0,0 +1,4 @@
+# Resource server(s)
+Je kunt 2 resource servers starten: 
+- Profile: default => 8085 / test
+- Profile: second => 8086 / test2
--- src/main/java/app/example/TestController.java
+++ src/main/java/app/example/TestController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	
 	@GetMapping("/test")
 	public String test() {
-		
-		return "success";
+		return "Keycloack CLIENT-CREDENTIALS secured resource gives: success";
+	}
+
+	@GetMapping("/test2")
+	public String test2() {
+		return "Keycloack USERNAME/PASSWORD secured resource gives: success";
 	}
 }
